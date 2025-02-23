c16e48c521338bfad818fcd60392264f021f9b79
Rohit87998 <phatak.rohit.temeb27@gmail.com>
2025-02-23 20:36:29 +0530

    third commit

--- src/DividendTracker.jsx
+++ src/DividendTracker.jsx
@@ -1,65 +1,65 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import LoginHeader from "./LoginHeader";
 import Banner from "./Banner";
-import './App.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
 
 const stocks = [
-  { name: "Zomato", sector: "Consumer Services", return: "5%" },
-  { name: "Tata Motors", sector: "Automobile", return: "2%" },
-  { name: "MRF", sector: "Tyres & Tubes", return: "4.4%" },
-  { name: "Infibeam Avenues", sector: "Information Technology", return: "1.25%" },
+  { name: "Zomato", sector: "Consumer Services", return: "5%", marketCap : "Large Cap" },
+  { name: "Tata Motors", sector: "Automobile", return: "2%", marketCap : "Mid Cap" },
+  { name: "MRF", sector: "Tyres & Tubes", return: "4.4%", marketCap : "Large Cap" },
+  { name: "Infibeam Avenues", sector: "Information Technology", return: "1.25%", marketCap : "Small cap" },
 ];
 
 export default function DividendTracker() {
   return (
     <>
-    <LoginHeader/>
-    <Banner/>
-    <div className="container-fluid p-4">
-      {/* Top Navigation */}
+      <LoginHeader />
+      <Banner />
+      <div className="container-fluid p-4">
+        <div className="row mt-4">
+          {/* Left Section - Search & Table */}
+          <div className="col-12 col-md-8 mb-4">
+            <div className="mb-3">
+              <div className="input-group">
+                <input type="text" className="form-control" placeholder="Search..." />
+                <span className="input-group-text">
+                  <i className="bi bi-search"></i>
+                </span>
+              </div>
+            </div>
+            <div className="table-responsive">
+              <table className="table table-striped table-bordered">
+                <thead className="table-dark">
+                  <tr>
+                    <th>Name</th>
+                    <th>Sector</th>
+                    <th>1 Year Return</th>
+                    <th>Market cap</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {stocks.map((stock, index) => (
+                    <tr key={index}>
+                      <td>{stock.name}</td>
+                      <td>{stock.sector}</td>
+                      <td>{stock.marketCap}</td>
+                      <td>{stock.return}</td>
 
-
-      {/* Header */}
- 
-
-      <div className="row mt-4">
-        {/* Left Section - Search & Table */}
-        <div className="col-md-8">
-          <div className="mb-3">
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="Search..." />
-              <span className="input-group-text">
-                <i className="bi bi-search"></i>
-              </span>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
-
-          <table className="table table-striped table-bordered">
-            <thead className="table-dark">
-              <tr>
-                <th>Name</th>
-                <th>Sector</th>
-                <th>1 Year Return</th>
-              </tr>
-            </thead>
-            <tbody>
-              {stocks.map((stock, index) => (
-                <tr key={index}>
-                  <td>{stock.name}</td>
-                  <td>{stock.sector}</td>
-                  <td>{stock.return}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
         </div>
-
-        {/* Right Section - Sidebar */}
-
       </div>
-    </div>
     </>
   );
 }
+
+// LoginHeader Component
+
+// Banner Component
+
